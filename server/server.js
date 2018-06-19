70e6de1e1ecc7e54231bf6cabbe8f1010d1f6214
unknown <michael.glushakov@gmail.com>
2018-06-19 19:26:52 -0400

    init

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path =require('path');
+const publicPath=path.join(__dirname+'/../public');
+const port=process.env.PORT||3000;
+const express = require('express');
+
+const app = express();
+app.use(express.static(publicPath));
+app.listen(port,() =>{
+  console.log(`Running on ${port}`);
+})
