3939fd11cc83d70803e63ae6c3952fed7eed293e
unknown <michael.glushakov@gmail.com>
2018-06-19 20:36:46 -0400

    socket basics

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+var socket=io();
+
+socket.on('connect',function(){
+  console.log('connected to server');
+
+
+});
+socket.on('disconnect',function(){
+  console.log('disconnected from the server');
+});
+
+socket.on('newMessage',function(message){
+  console.log('New Message',message);
+});
--- server/server.js
+++ server/server.js
@@ -14,10 +14,33 @@ io.on('connection',(socket)=>{
     console.log('New User Connected');
 
 
+    socket.emit('newMessage',{
+      from:'Admin',
+      text:'Welcome',
+      createdAt:new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+      from:'Admin',
+      text:'New USer joined',
+      createdAt:new Date().getTime()
+    });
+
+    socket.on('createMessage',(message)=>{
+      console.log('createMessage',message);
+      io.emit('newMessage',{
+        from:message.from,
+        text:message.text,
+        createdAt:new Date().getTime()
+      });
+
+    });
+
+
     socket.on('disconnect',(socket)=>{
         console.log('User Disconnected');
     });
 });
+
 
 
 
